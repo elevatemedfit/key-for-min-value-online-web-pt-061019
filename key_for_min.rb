ef4a1a6cdb073e0313d45241fc5690a33f8e7880
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-24 13:18:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-key_for_min_value.collect do |name, value|
+key_for_min_value.each do |name, value|
   nuname = ""
   nuvalue = ""
   if nuvalue =="" || value<nuvalue
